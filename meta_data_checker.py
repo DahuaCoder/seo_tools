4257852af886cf22561788a2e06e6a11b0919d03
Dario Steiner <dario.steiner@toradex.com>
2014-10-08 23:43:35 +0200

    Finished checker and prettifier

--- meta_data_checker.py
+++ meta_data_checker.py
@@ -31,12 +31,10 @@ def main():
     webpage_checker.gather_webpage_data()
 
     if args.check:
-        # MetaDataChecker.check_wepage_data()
-        pass
+        webpage_checker.check_webpage_data()
 
     if args.pretty:
-        # MataDataChecker.prettify_webpage_data()
-        pass
+        webpage_checker.prettify_webpage_data()
 
     webpage_checker.write_webpage_data_file()
 
@@ -44,6 +42,9 @@ def main():
 
 
 class MetaDataChecker(object):
+    MAX_TITLE_LENGTH = 69
+    MAX_DESCR_LENGTH = 160
+
     def __init__(self, urls):
         self.urls = urls
         self.pages = []
@@ -55,22 +56,56 @@ class MetaDataChecker(object):
         for url in self.urls:
             utility.show_progress()
             page = urlopen(url).read()
-            web_page = web_pages.WebPage(url, soup=BeautifulSoup(page))
-            page_data = []
-            page_data.append(web_page.get_title_tag())
-            page_data.append(web_page.get_meta_description())
-            page_data.append(url)
-            self.pages.append(page_data)
+            web_page = web_pages.WebPage(url)
+            web_page.setup_from_soup(BeautifulSoup(page))
+            self.pages.append(web_page)
 
         print('')
         print("All data received!")
 
     def write_webpage_data_file(self, file_name='output.csv'):
         """Writes all gathered web page datas into a csv file"""
+        config = ['title', 'description', 'url', 'comment']
         with open(file_name, 'w', encoding="utf-8") as file:
             writer = csv.writer(file, delimiter=';')
             writer.writerow(self.headers)
-            writer.writerows(self.pages)
+            for web_page in self.pages:
+                writer.writerow(web_page.get_printable_list(config))
+
+    def prettify_webpage_data(self):
+        """Prettifies data of web pages, i.e. removes newlines, tabs and spaces\
+        at end and beginning of file"""
+        for data in self.pages:
+            data.prettify_data()
+
+    def check_webpage_data(self):
+        """Check title tag and meta description lengths"""
+        no_errors = 0
+        for web_page in self.pages:
+            if web_page.has_title():
+                if len(web_page.title_tag) > self.MAX_TITLE_LENGTH:
+                    comment = "Title tag is too long (%d). It should be %d" %\
+                        (len(web_page.title_tag), self.MAX_TITLE_LENGTH)
+                    web_page.add_comment(comment)
+                    no_errors += 1
+            else:
+                web_page.add_comment("No title tag!")
+                no_errors += 1
+            if web_page.has_description():
+                if len(web_page.meta_description) > self.MAX_DESCR_LENGTH:
+                    comment = "Meta description is too long (%d). It should be %d" %\
+                        (len(web_page.meta_description), self.MAX_DESCR_LENGTH)
+                    web_page.add_comment(comment)
+                    no_errors += 1
+            else:
+                web_page.add_comment("No meta description!")
+                no_errors += 1
+
+        if no_errors > 0:
+            error_msg = "==> Found %d Errors in meta data" % no_errors
+            print(error_msg)
+        else:
+            print("==> No Errors found!")
 
 
 if __name__ == '__main__':
