9f92aa1127067e9b23bbd939186f9ff60779a164
Dario Steiner <dario.steiner@toradex.com>
2014-10-08 18:08:51 +0200

    Added meta_data_checker

--- meta_data_checker.py
+++ meta_data_checker.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3
 
 import file
+import web_pages
+import utility
 import argparse
 import sys
 from urllib.request import urlopen
@@ -8,53 +10,58 @@ from bs4 import BeautifulSoup
 
 
 def main():
-    description = """Reads in a file with a list of URLs, collects title tags and meta
-    descriptions of these websites and writes this data into a csv file. Additionally the quality
-    and length can be checked"""
+    description = """Reads in a file with a list of URLs, collects title tags
+    and meta descriptions of these websites and writes this data into a csv
+    file. Additionally the quality and length can be checked"""
     parser = argparse.ArgumentParser(description)
-    parser.add_argument('-i', '--input', dest='input_file', action='store', required=True,
+    parser.add_argument('-i', '--input', dest='input_file', action='store',
+                        required=True,
                         help='Specify input file or a single url')
     parser.add_argument('-c', '--check', action='store_true',
                         help='Flag to indicate whether data should be checked')
+    parser.add_argument('-p', '--pretty', action='store_true',
+                        help='Flag to indicate whether data should be\
+                        prettified, i.e. Removes all newlines, tabs, returns\
+                        and spaces at beginning and ending of data.')
 
     args = parser.parse_args()
-    input_file = file.File(args.input_file)
+    urls = file.get_lines_from_file(args.input_file)
+    webpage_checker = MetaDataChecker(urls)
+    webpage_checker.gather_webpage_data()
+
+    if args.check:
+        # MetaDataChecker.check_wepage_data()
+        pass
+
+    if args.pretty:
+        # MataDataChecker.prettify_webpage_data()
+        pass
+
+    # MeatDataChecker.write_webpage_data_file()
 
     return 0
 
 
 class MetaDataChecker(object):
     def __init__(self, urls):
-        self.web_pages = urls
+        self.urls = urls
+        self.pages = []
 
-    def get_web_pages(self, urls):
-        """Gets data from specified urls"""
+    def gather_webpage_data(self):
+        """Gathers data of all web pages specified in the url list"""
+        print("Getting web page data:")
+        for url in self.urls:
+            utility.show_progress()
+            page = urlopen(url).read()
+            web_page = web_pages.WebPage(BeautifulSoup(page))
+            page_data = []
+            page_data.append(web_page.get_title_tag())
+            page_data.append(web_page.get_meta_description())
+            page_data.append(url)
+            self.pages.append(page_data)
 
-
-def get_metadata(urls):
-    h1_titles = []
-    titles = []
-    descriptions = []
-    print("Getting websites ", end='')
-    for remote_url in urls:
-        show_progress()
-        page = urlopen(remote_url).read()
-        soup = BeautifulSoup(page)
-        h1 = soup.h1.string
-        h1_titles.append(make_pretty(h1))
-        title_tag = soup.title.string
-        titles.append(make_pretty(title_tag))
-        soup_meta_desc = soup.findAll('meta', attrs={"name": "description"})
-        descr = soup_meta_desc[0]['content']
-        descriptions.append(make_pretty(descr))
-
-    print('')
-    print('')
-    print("All data received!")
-
-    return((h1_titles, titles, descriptions))
-
-
+            print('')
+            print("All data received!")
 
 
 if __name__ == '__main__':
